aaa5e50215f08668ca18a10d854383c8faf52c3c
unknown <unknown@example.com>
2023-08-14 19:00:20 +0800

    更新后
    已实现单元格点击跳转，并隐藏自带顶部导航栏

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,8 +1,15 @@
 // index.js
-Page({})
 Component({
     data: {
+      mytext:"test",
       image: 'https://tdesign.gtimg.com/miniprogram/images/logo1.png',
       imageSrc: '/source/img1.jpg'
     },
+    methods:{
+        process: function(){
+            wx.navigateTo({
+              url: '/pages/page1/page1',
+            })
+        }
+    }
   })
