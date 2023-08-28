5186fe1fb20a60ea58083b40b0e1e7011751bd29
HoRizon <jiatenghuipl@gmail.com>
2023-08-28 15:02:24 +0800

    8.28
    额外添加人员选择功能，另需设置输入框缓存

--- pages/page3/page3.js
+++ pages/page3/page3.js
@@ -6,11 +6,21 @@ Page({
       dateVisible: false,
       date: new Date('2023-01-01').getTime(), // 支持时间戳传入
       dateText: '',
-  
+      personText: '',
+      personValue: [],
+      persons:[
+          {label:'李泽英', value:'李泽英'},
+          {label:'于耀钧', value:'于耀钧'},
+          {label:'李栋才', value:'李栋才'},
+          {label:'王星跃', value:'王星跃'},
+          {label:'王晓雪', value:'王晓雪'},
+          {label:'陈泓宇', value:'陈泓宇'}
+      ],
       // 指定选择区间起始值
       start: '2000-01-01 00:00:00',
       end: '2030-09-09 12:12:12',
     },
+      // 读取并载入缓存
     onShow: function(){
         let userInput = wx.getStorageSync('key')
         let date = wx.getStorageSync('date')
@@ -27,6 +37,29 @@ Page({
             })
         }
     },
+    onPickerConfirm(e){
+        const { key } = e.currentTarget.dataset;
+        const { value } = e.detail;
+        // console.log('picker change:', e.detail.value);
+        this.setData({
+          [`${key}Visible`]: false,
+          [`${key}Value`]: value,
+          [`${key}Text`]: value.join(' '),
+        });
+        console.log(value.toString())
+      },
+    onPickerCancel(e) {
+        const { key } = e.currentTarget.dataset;
+        console.log(e, '取消');
+        console.log('picker1 cancel:');
+        this.setData({
+            [`${key}Visible`]: false,
+        });
+        },
+    onPersonPicker() {
+        this.setData({ personVisible: true });
+        },
+
     inputChange(e){
         console.log(e)
         console.log(this.data)
@@ -41,7 +74,8 @@ Page({
             inputValue: '',
             dateText:''
         })
-        wx.clearStorageSync()
+        wx.removeStorageSync('key')
+        wx.removeStorageSync('date')
     },
     // 导航栏回退处理逻辑
     handleBack() {
